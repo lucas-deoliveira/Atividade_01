83b3eb976ea2a970941296e03421710e7e9fe963
marcos-michalski <marcos-michalski@hotmail.com.br>
2020-03-19 00:24:09 -0300

    Criação da classe Bascara

--- eng2020_1_a01/src/eng2020_1_a01/Bascara.java
+++ eng2020_1_a01/src/eng2020_1_a01/Bascara.java
@@ -1,6 +1,19 @@
 package eng2020_1_a01;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Bascara {
+    private double valueA;
+    private double valueB;
+    private double valueC;
 
-
+    public List calculateBascara() {
+        List result = new ArrayList();
+        double X1 = (-this.valueB + Math.sqrt( Math.pow(this.valueB, 2) - (4 * this.valueA * this.valueC) ) ) / (2 * this.valueA);
+        double X2 = (-this.valueB - Math.sqrt( Math.pow(this.valueB, 2) - (4 * this.valueA * this.valueC) ) ) / (2 * this.valueA);
+        result.add(X1);
+        result.add(X2);
+        return result;
+    }
 }
